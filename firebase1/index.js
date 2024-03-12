458b048ffef33d2d559c190a0a0ea4f6d6657a07
Mathieu Giguere <matduke@gmail.com>
2024-03-11 22:02:39 -0400

    e

---
+++ firebase1/index.js
@@ -0,0 +1,6 @@
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.gelElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    console.log("it works")
+})
