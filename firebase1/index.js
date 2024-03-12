bed1d4bd1ed28ac0c556e0c493e99c76f8ae67d5
Mathieu Giguere <matduke@gmail.com>
2024-03-11 23:04:58 -0400

    a

--- firebase1/index.js
+++ firebase1/index.js
@@ -1,6 +1,7 @@
 import { add } from "./functions.js"
 
-console.log(add(1,1))
+console.log(add(1,2))
+
 
 const appSettings = {
     databaseURL: "https://playground-e67a2-default-rtdb.firebaseio.com/"
