be0e1e20bc842bba6c82bb4b32445f84506d4297
Mathieu Giguere <matduke@gmail.com>
2024-03-11 22:08:17 -0400

    a

--- firebase1/index.js
+++ firebase1/index.js
@@ -2,5 +2,7 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
-    console.log("inputFieldEl.value")
+    let inputValue = inputFieldEl.value
+
+    console.log(inputValue)
 })
