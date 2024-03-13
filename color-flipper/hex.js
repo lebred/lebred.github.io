344882dff423c0defba0586f3c936087ea215daf
Mathieu Giguere <matduke@gmail.com>
2024-03-12 23:47:00 -0400

    a

--- color-flipper/hex.js
+++ color-flipper/hex.js
@@ -4,5 +4,10 @@ const btn = document.getElementById('btn')
 const color = document.getElementById(".color")
 
 btn.addEventListener('click', function(){
-    
+    let hexColor = "x";
+    for(let i = 0; i < 6;i++){
+        hexColor += hex[0]
+    }
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hex;
 })
