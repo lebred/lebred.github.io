ade40598ee2521c8de4222ae0cb0c219e4837572
Mathieu Giguere <matduke@gmail.com>
2024-03-16 14:05:42 -0400

    a

--- counter/app.js
+++ counter/app.js
@@ -2,4 +2,7 @@
 let count = 0;
 
 // select value and buttons
-const value = document.querySelector('#value')
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+console.log(btns);
