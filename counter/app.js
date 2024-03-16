23534360e654617e58543097e1db8f6eb6aee5e4
Mathieu Giguere <matduke@gmail.com>
2024-03-16 14:08:20 -0400

    a

--- counter/app.js
+++ counter/app.js
@@ -5,6 +5,8 @@ let count = 0;
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function(orange) {
-    console.log(orange);
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e){
+        console.log();
+    })
 });
