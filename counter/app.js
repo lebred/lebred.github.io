f409533b21f9e3b4213e48aa9457d0a59d364964
Mathieu Giguere <matduke@gmail.com>
2024-03-16 14:12:09 -0400

    a

--- counter/app.js
+++ counter/app.js
@@ -7,6 +7,16 @@ const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function(btn) {
     btn.addEventListener("click", function(e){
-        const styles = e.currentTarget.classList
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
     })
 });
