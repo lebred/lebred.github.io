6f0625f62717c3a3ddd459c0527330cd3768ec06
Mathieu Giguere <matduke@gmail.com>
2024-03-17 13:48:30 -0400

    e

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -4,6 +4,15 @@
 // when user clicks close-btn remove .open-modal from modal-overlay
 
 
-const modalBtn = document.querySelector("modal-btn")
-const closeBtn = document.querySelector("close-btn")
-const overlay = document.querySelector("modal-overlay")
+const modalBtn = document.querySelector(".modal-btn")
+const closeBtn = document.querySelector(".close-btn")
+const overlay = document.querySelector(".modal-overlay")
+
+modalBtn.addEventListener('click', function(){
+    overlay.classList.add("open-modal");
+});
+
+closeBtn.addEventListener('click', function(){
+    overlay.classList.remove("open-modal");
+});
+
