6df3c0da4e22abc9c324468bb7066196e65d6a1b
Mathieu Giguere <matduke@gmail.com>
2024-03-17 13:44:50 -0400

    e

--- 06-modal/app.js
+++ 06-modal/app.js
@@ -2,3 +2,8 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+
+const modalBtn = document.querySelector("modal-btn")
+const closeBtn = document.querySelector("close-btn")
+const overlay = document.querySelector("modal-overlay")
