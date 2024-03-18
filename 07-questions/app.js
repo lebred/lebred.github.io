b3dfe6938102d676c9380fd82150b356897c9349
lebred <matduke@gmail.com>
2024-03-17 21:04:55 -0400

    a

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,8 +1,14 @@
-const btns = document.querySelectorAll('.question-btn');
+// const btns = document.querySelectorAll('.question-btn');
 
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-        const question = e.currentTarget.parentElement.parentElement 
-        question.classList.toggle('show-text');
-        })
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement 
+//         question.classList.toggle('show-text');
+//         })
+// })
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(orange){
+    console.log(orange)
 })
