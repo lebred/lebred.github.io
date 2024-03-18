bbfc12052abc74676579a706de712369258227e7
lebred <matduke@gmail.com>
2024-03-17 21:11:02 -0400

    e

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -11,5 +11,7 @@ const questions = document.querySelectorAll('.question');
 
 questions.forEach(function(question){
     const btn = question.querySelector('.question-btn')
-    console.log(btn);
+    btn.addEventListener("click", function*(){
+        question.classList.toggle("show-text")
+    })
 })
