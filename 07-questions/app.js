677267da39027971b613e808b4a72242f4d9f153
Mathieu Giguere <matduke@gmail.com>
2024-03-17 20:37:51 -0400

    e

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,2 +1,7 @@
 const btns = document.querySelectorAll('.question-btn');
 
+btns.forEach(function(){
+    btn.addEventListener('click', function(e){
+        console.log(e.currentTarget);
+    })
+})
