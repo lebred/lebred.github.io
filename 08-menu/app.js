e797c498f22c1674aed16bdf6e60950a0a6bea3d
Mathieu Giguere <matduke@gmail.com>
2024-03-24 13:40:17 -0400

    e

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -85,9 +85,13 @@ window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
 });
 
-
-
 // filter items
+
+filterBtns.forEach(function(btn){
+  btn.addEventListener('click', function(e){
+    console.log(e.currentTarget.dataset);
+  })
+})
 
 function displayMenuItems(menuItems){
   let displayMenu = menu.map(function(item){
