9a80fd4c76428d0afa65a6260827a365461e43eb
Mathieu Giguere <matduke@gmail.com>
2024-03-24 13:01:49 -0400

    e

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -72,3 +72,7 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+
+const sectionCenter = document.querySelector('.section-center');
+
