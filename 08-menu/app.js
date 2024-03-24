5494068ae7bfa06d1ecf5a9873efc932e12bbe2b
Mathieu Giguere <matduke@gmail.com>
2024-03-24 13:33:51 -0400

    e

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -77,25 +77,26 @@ const menu = [
 const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function(){
-  let displayMenu = menu.map(function(item){
-    // console.log(item);
-
-    return `<article class="menu-item">
-        <img src="${item.img}" class="photo" alt="${item.title}">
-        <div class="item-info">
-          <header>
-            <h4>${item.title}</h4>
-            <h4 class="price">$${item.price}</h4>
-          </header>
-          <p class="item-text">${item.desc}</p>
-        </div>
-      </article>`;
-  })
-  displayMenu = displayMenu.join("");
-  sectionCenter.innerHTML = displayMenu;
-  // console.log(displayMenu);
+  displayMenuItems(menu);
 });
 
 function displayMenuItems(menuItems){
-  
+  let displayMenu = menu.map(function(item){
+      // console.log(item);
+
+      return `<article class="menu-item">
+          <img src="${item.img}" class="photo" alt="${item.title}">
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">$${item.price}</h4>
+            </header>
+            <p class="item-text">${item.desc}</p>
+          </div>
+        </article>`;
+    })
+    displayMenu = displayMenu.join("");
+    sectionCenter.innerHTML = displayMenu;
+  // console.log(displayMenu);
+
 }
