61918e13c9d2e459a94fef781d7ed3eba93df9a2
Mathieu Giguere <matduke@gmail.com>
2024-03-24 14:19:08 -0400

    e

--- 08-menu/app.js
+++ 08-menu/app.js
@@ -92,7 +92,11 @@ const filterBtns = document.querySelectorAll('.filter-btn');
 window.addEventListener('DOMContentLoaded', function(){
   displayMenuItems(menu);
 
-  const categories = menu.reduce(function(){
+  const categories = menu.reduce(function(values, item){
+    if(!values.includes(item.category)){
+      values.push(item.category)
+    }
+    return values;
   },['all'])
   console.log(categories);
 });
