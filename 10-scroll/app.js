0f955b515d5c29b599d5c5ccdfb76a59d4476963
Mathieu Giguere <matduke@gmail.com>
2024-03-27 10:54:55 -0400

    e

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -5,6 +5,9 @@
 
 // ********** set date ************
 
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
 // ********** close links ************
 
 // ********** fixed navbar ************
