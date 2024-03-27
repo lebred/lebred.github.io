8d9ff2219cdbe4775d3173661cd2c8e7db4603da
Mathieu Giguere <matduke@gmail.com>
2024-03-27 11:02:46 -0400

    e

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -10,12 +10,12 @@ date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
 
-const navToggle = document.querySelector('nav-toggle');
-const linksContainer = document.querySelector('links-container');
-const links = document.querySelector('links');
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function(){
-    
+    linksContainer.classList.toggle('show-links');
 })
 
 // ********** fixed navbar ************
