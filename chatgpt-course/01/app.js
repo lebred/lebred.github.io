7c27dd952d11af24682d99270dea985cb2ef2b8e
lebred <matduke@gmail.com>
2024-03-27 22:18:47 -0400

    e

--- chatgpt-course/01/app.js
+++ chatgpt-course/01/app.js
@@ -2,7 +2,7 @@ async function fetchData(){
     const response = await fetch("https://api.openai.com/v1/completions", {
         method: "POST",
         headers: {
-            Authorization: `Bearer ${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}`,
+            Authorization: `Bearer ${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}`,
             "Content-type": "application/json"
         },
         body: JSON.stringify({
