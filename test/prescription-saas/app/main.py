d6079b2a147be2ababaf0e57b5a9f6c21d5f49d9
Mathieu Giguere <matduke@gmail.com>
2024-08-21 23:20:47 -0400

    a

--- test/prescription-saas/app/main.py
+++ test/prescription-saas/app/main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to the Prescription SaaS!"
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
